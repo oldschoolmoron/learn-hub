e017aca7cf368bb01c4eddebeef2e62942ffbd83
Ananya Pathak <anyalko043@gmail.com>
2025-01-10 19:45:07 +0530

    Added Outlet for rendering child routes in parent layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,34 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { BrowserRouter, Routes, Route, Link, useNavigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
 
 function App() {
   return <>
     <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Landing />} />
+          <Route path="/neet/online-coaching-class-11" element={<Class11Program />} />
+          <Route path='/neet/online-coaching-class-12' element={<Class12Program />} />
+          <Route path="*" element={<ErrorPage />} />
+      </Route>
+    </Routes>
+      Footer part | contact us
+  </BrowserRouter>
+    </>
+}
+
+function Layout(){
+  return <div>
     <Link to={"/"}> Allen</Link>
     |
     <Link to={"/neet/online-coaching-class-11"}>Class 11</Link>
     |
     <Link to={"/neet/online-coaching-class-12"}>Class 12</Link>
-      <Routes>
-        <Route path="/neet/online-coaching-class-11" element={<Class11Program />} />
-        <Route path='/neet/online-coaching-class-12' element={<Class12Program />} />
-        <Route path="*" element={<ErrorPage />} />
-        <Route path='/' element={<Landing />} />
-      </Routes>
-    </BrowserRouter>
-    </>
+    Hi there
+    <Outlet />
+  </div>
 }
 
 function ErrorPage(){
